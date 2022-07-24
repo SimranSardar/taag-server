265c9b5e049b2617fd1799bbc8e70991a1d22692
Anurag Pal <apal895@gmail.com>
2022-07-25 00:59:10 +0530

    changes

---
+++ models/User.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }, // Hash
+    userType: { type: String, required: true }, // team | brand | admin
+    createdAt: { type: String, required: true }, // ISOString
+    updatedAt: { type: String, required: true }, // ISOString
+  },
+  { collection: "users" }
+);
