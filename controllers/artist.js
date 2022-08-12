7c9e8034682645c9b7af13b049f53998e2d361cb
Anurag Pal <apal895@gmail.com>
2022-08-12 11:45:03 +0530

    categories

--- controllers/artist.js
+++ controllers/artist.js
@@ -219,7 +219,7 @@ export async function uploadArtistExcel(req, res) {
       manager: item.manager || item.agencyName || "NA",
       contact: parseInt(item.contact) || 0,
       categories:
-        (item?.categories.includes(",")
+        (item?.categories?.includes(",")
           ? item.categories.split(",").map((item) => item.trim())
           : item.categories.split("/").map((item) => item.trim())) || [],
       createdAt: new Date().toISOString(),
