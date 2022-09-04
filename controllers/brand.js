5c744286eacdf3100755df5a8123c173263ed353
Anurag Pal <apal895@gmail.com>
2022-09-04 19:31:43 +0530

    check brand already exists

--- controllers/brand.js
+++ controllers/brand.js
@@ -3,10 +3,10 @@ import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
 
 export async function createBrand(req, res) {
-  const { name, email, password } = req.body;
+  const { name, poc, password } = req.body;
 
   try {
-    const newUser = await BrandModel.findOne({ "poc.email": email });
+    const newUser = await BrandModel.findOne({ "poc.email": poc.email });
 
     if (newUser) {
       return res.status(400).json({ message: "Email already exists" });
